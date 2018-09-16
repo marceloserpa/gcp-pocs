201c5d683c4d25edfd5713f23e7ee442e40a019c
Marcelo Serpa <marcelo.serpa@ilegra.com>
2018-09-16 16:29:28 -0300

    remove invalid endpoint

--- nodejs-appengine-standard/index.js
+++ nodejs-appengine-standard/index.js
@@ -10,15 +10,6 @@ app.get('/api/books', (req, res) => {
   res.send(books);
 });
 
-app.get('/api/books222', (req, res) => {
-  let books = [{
-    title: "The shining",
-    author: "Stephen King"
-  }];
-
-  res.send(books);
-});
-
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
