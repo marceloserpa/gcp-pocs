20a9803384b6e92e086786e379d1c463bee0f5ee
Marcelo Serpa <marcelo.serpa@ilegra.com>
2019-01-16 22:50:45 -0200

    adding support Firestore

--- gae-java8-standard-crud/src/main/java/com/bookstore/BookService.java
+++ gae-java8-standard-crud/src/main/java/com/bookstore/BookService.java
@@ -1,65 +1,88 @@
 package com.bookstore;
 
-import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
-import com.google.api.server.spi.response.NotFoundException;
-import com.google.appengine.api.users.User;
-
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import javax.inject.Named;
 
+import com.google.api.core.ApiFuture;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.response.NotFoundException;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+
 //http://localhost:8080/_ah/api/discovery/v1/apis/
-@Api(
-    name = "books",
-    version = "v1"
-    /*,
-    scopes = {Constants.EMAIL_SCOPE},
-    clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
-    audiences = {Constants.ANDROID_AUDIENCE}*/
+@Api(name = "books", version = "v1"
+/*
+ * , scopes = {Constants.EMAIL_SCOPE}, clientIds = {Constants.WEB_CLIENT_ID,
+ * Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID}, audiences =
+ * {Constants.ANDROID_AUDIENCE}
+ */
 )
 public class BookService {
 
-  public static ArrayList<Book> books = new ArrayList<Book>();
+	public static ArrayList<Book> books = new ArrayList<Book>();
 
-  static {
-    books.add(new Book("The Shining"));
-    books.add(new Book("Under the domme"));
-  }
+	static {
+		books.add(new Book("The Shining"));
+		books.add(new Book("Under the domme"));
+	}
 
-  //http://localhost:8080/_ah/api/books/v1/book/1
-  public Book getBook(@Named("id") Integer id) throws NotFoundException {
-    try {
-      return books.get(id);
-    } catch (IndexOutOfBoundsException e) {
-      throw new NotFoundException("Greeting not found with an index: " + id);
-    }
-  }
+	// http://localhost:8080/_ah/api/books/v1/book/1
+	public Book getBook(@Named("id") Integer id) throws NotFoundException {
+		try {
+			return books.get(id);
+		} catch (IndexOutOfBoundsException e) {
+			throw new NotFoundException("Greeting not found with an index: " + id);
+		}
+	}
 
-  //http://localhost:8080/_ah/api/books/v1/book
-  public ArrayList<Book> listBook() {
-    return books;
-  }
+	// http://localhost:8080/_ah/api/books/v1/book
+	public ArrayList<Book> listBook() throws InterruptedException, ExecutionException {
+		FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId("MY-PROJECT")
+				.build();
+		Firestore db = firestoreOptions.getService();
 
-  @ApiMethod(name = "book", httpMethod = "post")
-  public Book insertGreeting(Book book) {
-	  /**
-    Book response = new Book();
-    StringBuilder responseBuilder = new StringBuilder();
-    for (int i = 0; i < times; i++) {
-      responseBuilder.append(greeting.getMessage());
-    }
-    response.setMessage(responseBuilder.toString());
-    */
-	books.add(book);
-	Book response = new Book();
-	response.setMessage("ok");
-    return response;
-  }
+		// asynchronously retrieve all users
+		ApiFuture<QuerySnapshot> query = db.collection("books").get();
+		// ...
+		// query.get() blocks on response
+		QuerySnapshot querySnapshot = query.get();
+		List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+		books = new ArrayList<>();
+		for (QueryDocumentSnapshot document : documents) {
+			Book book = new Book();
+			book.setMessage(document.getString("author"));
+			books.add(book);
 
-  @ApiMethod(name = "greetings.authed", path = "hellogreeting/authed")
-  public Book authedGreeting(User user) {
-    Book response = new Book("hello " + user.getEmail());
-    return response;
-  }
+		}
+
+		return books;
+	}
+
+	@ApiMethod(name = "book", httpMethod = "post")
+	public Book insertGreeting() throws InterruptedException, ExecutionException {
+		FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId("MY_PROJECT")
+				.build();
+		Firestore db = firestoreOptions.getService();
+		DocumentReference docRef = db.collection("books").document("theshining");
+		Map<String, Object> data = new HashMap<>();
+		data.put("author", "Stephen King");
+		data.put("title", "The Shining");
+		data.put("category", "Horror");
+		ApiFuture<WriteResult> result = docRef.set(data);
+		result.get();
+		Book response = new Book();
+		response.setMessage("ok");
+		return response;
+	}
+
 }
